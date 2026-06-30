0a22aef164ffd0ddd1e8b54ac7090c60dc0b59cc
Felipe Benatti <fepabenattinho@gmail.com>
2026-06-30 16:10:19 -0300

    chore: update APPS_SCRIPT_URL after anti-spam redeploy

--- config.js
+++ config.js
@@ -6,4 +6,4 @@
 // ============================================
 
 // Usando o web app já deployado
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwIlFedzButbrbEkoXJtV3O-HNG5JKewTslzdhHrkHLU96bmJFq2Ug5ywaMeNrH8gpR/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxpP3jOZ4PpzcV3qgz_3JA84i1bX7cMvHhddUot3iSTDjVg5KQikWdogpViGtwh-BBN/exec';
